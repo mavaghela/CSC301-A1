993ae7d82261f7df9544a3d9981a312d80d75aca
ProfDema <ilir.dema@mail.utoronto.ca>
2017-01-07 18:28:26 -0500

    Starter Code for A1

---
+++ Grid.java
@@ -0,0 +1,47 @@
+package a1;
+
+public class Grid
+{
+protected int rows;         // number of grid rows
+protected int cols;         // number of grid columns
+ 
+protected boolean [][] grid;     // the grid containing blobs
+// You may define helper variables as needed
+
+public Grid(int rows, int cols, boolean[][] grid)
+
+{
+ this.rows = rows;
+ this.cols = cols;
+ this.grid = grid;
+ 
+}
+
+public String toString()
+{
+ String gridString = "";
+ for (int i = 0; i < rows; i++)
+ {
+   for (int j = 0; j < cols; j++)
+   {
+     if (grid[i][j])
+       gridString = gridString + "X";
+     else
+       gridString = gridString + ".";
+    }
+   gridString = gridString + "\n";   // end of row
+ }  
+ return gridString;
+}
+
+public int blobCount()
+// returns the number of blobs in this grid
+{
+ int count = 0;
+ // Your code goes here
+ return count;
+}
+
+// You may define heper methods as needed
+}
+
